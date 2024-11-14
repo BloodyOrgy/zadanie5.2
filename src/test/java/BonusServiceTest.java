35ea69ec04b6ff32827aefdb5628166cc59294b6
BloodyOrgy <rad_devil@inbox.ru>
2024-11-14 19:42:43 +0400

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -67,4 +67,36 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+    
+        @Test
+    void shouldCalculateForRegisteredAndLessLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 30;
+        boolean registered = true;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForNotRegistered() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = false;                ;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
 }
